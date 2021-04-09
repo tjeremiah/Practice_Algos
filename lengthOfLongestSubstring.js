ca1c72301bbbfdac3a05549ed61d936011da03b2
Torrel <taurel107@gmail.com>
2021-04-08 22:37:47 -0400

    most optimized solution

--- lengthOfLongestSubstring.js
+++ lengthOfLongestSubstring.js
@@ -26,3 +26,28 @@ var lengthOfLongestSubstring = function(s) {
 console.log(`Length of the longest substring: ${lengthOfLongestSubstring('aabccbb')}`);
 console.log(`Length of the longest substring: ${lengthOfLongestSubstring('abbbb')}`);
 console.log(`Length of the longest substring: ${lengthOfLongestSubstring('abccde')}`);
+
+//Most optimized solution:
+
+const function lengthOfLongestSubstring(str) {
+    let charsArray = new Array(128),
+     start = 0,
+     end = 0,
+     maxLength = 0;
+
+     while (end < str.length) {
+        let charValue = s.charCodeAt(start);
+         
+        let index = charsArray[charValue];
+
+        if (index !== null && index >= end && index < start) {
+           start = index + 1;
+        }
+
+        maxLength = Math.max(maxLength, end - start + 1)
+
+        charArray[charValue] = end;
+        end++;
+     }
+
+}
