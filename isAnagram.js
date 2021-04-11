63447a37fecb4c98cd7103ef2bbeef37f8497a77
Torrel <taurel107@gmail.com>
2021-04-10 21:30:19 -0400

    isAnagram Practice

---
+++ isAnagram.js
@@ -0,0 +1,64 @@
+// Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+
+var isAnagram = function(s, t) {
+
+    let s_Obj = {},
+     t_Obj = {};
+     
+      if(s.length != t.length) {
+        return false;
+      }
+ 
+     for(let i = 0; i < s.length; i++) {
+         let char = s[i];
+         if(!(char in s_Obj)) {
+             s_Obj[char] = 0;
+         }
+         s_Obj[char] += 1;
+         
+         let rightChar = t[i]
+         if(!(rightChar in t_Obj)) {
+             t_Obj[rightChar] = 0
+         }
+         t_Obj[rightChar] += 1;
+     }
+ 
+      for (let key in  s_Obj){
+           if(s_Obj[key] != t_Obj[key]) {
+              return false
+            }
+       }
+       return true;
+ }
+ 
+ 
+ 
+ console.log(isAnagram("aacc","ccac"))  //false
+ console.log(isAnagram("anagram", "nagaram")) //true
+ console.log(isAnagram("ab", "a")) //false
+
+// Optimal solution
+
+function isAnagram(s, t) {
+
+    const countArr = new Array(26).fill(0) //allocating space for the 26 chars of the alphabet
+
+    if (s.length != t.length) {
+        return false
+    }
+    
+    for (let char of s) {
+        //take the string'char' return the unicode of the firstchar and subtract from 97 - this is the index, then increment by 1
+        countArr[char.charCodeAt(0) - 97] +=1;
+    }
+
+    for (let char of t) {
+        countArr[char.charCodeAt(0) - 97] += 1;
+    }
+
+    return countArr.every(element => element === 0)
+}
+
+console.log(isAnagram("aacc","ccac"))  //false
+ console.log(isAnagram("anagram", "nagaram")) //true
+ console.log(isAnagram("ab", "a")) //false
--- subArraySum.js
+++ subArraySum.js
@@ -1,34 +1,34 @@
-// function subArraySum(arr, k) {
+function subArraySum(arr, k) {
 //   //increment a counter, to track when some part of the array has added up to k
-//   // take a note of it
-//   let sum = 0,
-//   // here is where i'll take note of when my addition is equal k 
-//    count = 0,
-//   //will also need a place to store my sum.
-//    cache = {};
+  // take a note of it
+  let sum = 0,
+  // here is where i'll take note of when my addition is equal k 
+   count = 0,
+  //will also need a place to store my sum.
+   cache = {};
   
-//    for (let i = 0; i < arr.length; i++) {
-//       sum += arr[i];
+   for (let i = 0; i < arr.length; i++) {
+      sum += arr[i];
 
-//       if(sum == k) {
-//          count += 1
-//       }
+      if(sum == k) {
+         count += 1;
+      }
 
-//       if((sum - k) in cache) {
-//           count += cache[sum -k]
-//       }
+      if((sum - k) in cache) {
+          count += cache[sum -k];
+      }
 
-//       if(sum in cache) {
-//          cache[sum] += 1;
-//       }
-//       else {
-//           cache[sum] = 1;
-//       }
-//     }
-//     return count;
+      if(sum in cache) {
+         cache[sum] += 1;
+      }
+      else {
+          cache[sum] = 1;
+      }
+    }
+    return count;
 
-// }
-// console.log(subArraySum([3,4,7,2,-3,1,4,2], 7));
+}
+console.log(subArraySum([3,4,7,2,-3,1,4,2], 7));
 
 //Optimal solution
 
