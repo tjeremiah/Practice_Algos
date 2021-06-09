a1090cacadf245d2995b5f8b63ee6a04237b2e38
Torrel <taurel107@gmail.com>
2021-06-09 11:00:54 -0400

    adding maxDepth.js

---
+++ strings/maxDepth.js
@@ -0,0 +1,21 @@
+//Given a VPS (Valid parentheses string) represented as string s, return the nesting depth of s.
+
+const maxDepth = str => {
+   let depth = 0;
+   let maxDepth = 0;
+
+   for(char of str) {
+       if (char === '(' ) {
+           depth += 1;
+           if (depth > maxDepth) {
+               maxDepth = depth;
+           }
+       }
+       else if ( char === ')' ) {
+           depth -= 1;
+       }
+   }
+   return maxDepth;
+}
+
+console.log(maxDepth("(1+(2*3)+((8)/4))+1"))
