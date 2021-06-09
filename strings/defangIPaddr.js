c5a584a544afecd64e08cb0865a265b47fef52fd
Torrel <taurel107@gmail.com>
2021-06-08 22:47:20 -0400

    defangIPaddr

---
+++ strings/defangIPaddr.js
@@ -0,0 +1,11 @@
+//Input: address = "255.100.50.0"
+//Output: "255[.]100[.]50[.]0"
+
+const defangIPaddr = str => {
+    let new_address = "";
+     for (let i of address) {
+         new_address += (i === ".") ? '[.]' : i)
+
+     }
+     return new_address
+}
