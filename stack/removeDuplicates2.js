1a4e63ee3635f02fda44cece5d6b6afeaf72ba2b
Torrel <taurel107@gmail.com>
2021-06-29 23:59:06 -0400

    adding duplicates2

---
+++ stack/removeDuplicates2.js
@@ -0,0 +1,12 @@
+//Input: s = "deeedbbcccbdaa", k = 3
+//Output: "aa"
+//Explanation: 
+//First delete "eee" and "ccc", get "ddbbbdaa"
+//Then delete "bbb", get "dddaa"
+//Finally delete "ddd", get "aa"
+
+const removeDuplicates = (s, k) {
+    let stack = [];
+
+}
+console.log(removeDuplicates("deeedbbcccbdah",3))
